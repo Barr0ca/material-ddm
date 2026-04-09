af8db40c5309d1fa618482b69dad502d6f024d37
iandearaujogalvao@gmail.com <iandearaujogalvao@gmail.com>
2026-04-09 08:38:43 -0300

    fix: Modificando App para prática 04

--- app-mobile-v2/App.tsx
+++ app-mobile-v2/App.tsx
@@ -1,21 +1,43 @@
 import { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
 import { CardAluno } from './src/components/CardAluno';
+import { CardParticipacao } from './src/components/CardParticipacao';
+import { CabecalhoAula } from './src/components/CabecalhoAula';
 
 export default function App() {
-  const [presente, setPresente] = useState(false);
+  const [participacao, setParticipacao] = useState(1);
+  var [mensagem, setMensagem] = useState('Vamos começar?');
 
-  function alternarPresenca() {
-    setPresente((valorAtual) => !valorAtual);
+
+  function incrPart() {
+    setParticipacao(participacao + 1);
+  }
+  function zerarPart() {
+    setParticipacao(0);
+  }
+
+  if (participacao === 0) {
+    mensagem = 'Vamos começar?'
+  } else if (participacao < 4) {
+    mensagem = 'Boa participação!'
+  } else if (participacao >= 4) {
+    mensagem = 'Excelente ritmo hoje!'
   }
 
   return (
     <View style={styles.container}>
-      <CardAluno
-        nome="Aluno Exemplo"
-        presente={presente}
-        onAlternarPresenca={alternarPresenca}
+      <CabecalhoAula 
+        titulo='Aula 04'
+        subtitulo='Aula de hoje apredemos props.'
       />
+      <CardParticipacao 
+        nome='Ian'
+        participacoes={participacao}
+        mensagem={mensagem}
+        maisParticipacao={incrPart}
+        zerarParticipacao={zerarPart}
+      />
+
     </View>
   );
 }
@@ -27,4 +49,10 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#f3f4f6',
   },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1
+  }
 });
