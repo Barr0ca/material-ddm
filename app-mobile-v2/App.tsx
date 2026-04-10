f503d4d34dc8fbe4b2651d1c204dabc9430dcc98
iandearaujogalvao@gmail.com <iandearaujogalvao@gmail.com>
2026-04-10 10:05:09 -0300

    fix: Testando componente sobre stylesheet

--- app-mobile-v2/App.tsx
+++ app-mobile-v2/App.tsx
@@ -1,43 +1,11 @@
-import { useState } from 'react';
-import { StyleSheet, View, TextInput, Text } from 'react-native';
-import { CardAluno } from './src/components/CardAluno';
-import { CardParticipacao } from './src/components/CardParticipacao';
-import { CabecalhoAula } from './src/components/CabecalhoAula';
+import { StyleSheet, View, Text } from 'react-native';
+import { StyleBoaPratica } from './src/components/StyleBoaPratica';
+
 
 export default function App() {
-  const [participacao, setParticipacao] = useState(1);
-  var [mensagem, setMensagem] = useState('Vamos começar?');
-
-
-  function incrPart() {
-    setParticipacao(participacao + 1);
-  }
-  function zerarPart() {
-    setParticipacao(0);
-  }
-
-  if (participacao === 0) {
-    mensagem = 'Vamos começar?'
-  } else if (participacao < 4) {
-    mensagem = 'Boa participação!'
-  } else if (participacao >= 4) {
-    mensagem = 'Excelente ritmo hoje!'
-  }
-
   return (
     <View style={styles.container}>
-      <CabecalhoAula 
-        titulo='Aula 04'
-        subtitulo='Aula de hoje apredemos props.'
-      />
-      <CardParticipacao 
-        nome='Ian'
-        participacoes={participacao}
-        mensagem={mensagem}
-        maisParticipacao={incrPart}
-        zerarParticipacao={zerarPart}
-      />
-
+      <StyleBoaPratica />
     </View>
   );
 }
@@ -45,14 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#f3f4f6',
+    padding: 32,
+    gap: 12,
+    backgroundColor: "#6f85ffff",
   },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1
-  }
 });
